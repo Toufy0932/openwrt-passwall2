e16c3c2eeee56511693149936cf2ea672dd4e764
Toufy <60051694+Toufy0932@users.noreply.github.com>
2025-06-04 11:49:08 +0800

    Update com.lua

--- luci-app-passwall2/luasrc/passwall2/com.lua
+++ luci-app-passwall2/luasrc/passwall2/com.lua
@@ -8,21 +8,6 @@ local function gh_pre_release_url(self)
 	return "https://api.github.com/repos/" .. self.repo .. "/releases?per_page=1"
 end
 
-_M.hysteria = {
-	name = "Hysteria",
-	repo = "HyNetwork/hysteria",
-	get_url = gh_release_url,
-	cmd_version = "version | awk '/^Version:/ {print $2}'",
-	remote_version_str_replace = "app/",
-	zipped = false,
-	default_path = "/usr/bin/hysteria",
-	match_fmt_str = "linux%%-%s$",
-	file_tree = {
-		armv6 = "arm",
-		armv7 = "arm"
-	}
-}
-
 _M["sing-box"] = {
 	name = "Sing-Box",
 	repo = "SagerNet/sing-box",
@@ -34,23 +19,6 @@ _M["sing-box"] = {
 	match_fmt_str = "linux%%-%s",
 	file_tree = {
 		x86_64 = "amd64",
-		mips64el = "mips64le"
-	}
-}
-
-_M.xray = {
-	name = "Xray",
-	repo = "XTLS/Xray-core",
-	get_url = gh_pre_release_url,
-	cmd_version = "version | awk '{print $2}' | sed -n 1P",
-	zipped = true,
-	default_path = "/usr/bin/xray",
-	match_fmt_str = "linux%%-%s",
-	file_tree = {
-		x86_64 = "64",
-		x86    = "32",
-		mips   = "mips32",
-		mipsel = "mips32le",
 		mips64el = "mips64le"
 	}
 }
